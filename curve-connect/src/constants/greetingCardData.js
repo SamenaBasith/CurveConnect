6505f98ec945d8a60ef70717237a10ee765a1ba2
EdLakeNHS <edward.lake1@nhs.net>
2025-11-24 13:26:26 +0000

    Linked card images into constant named greetingCardData

---
+++ curve-connect/src/constants/greetingCardData.js
@@ -0,0 +1,34 @@
+const greetingCardData = [
+    {
+        id: 'card-1',
+        src: '/thank-you-card-1.png',
+        alt: "I'm ever so grateful!",
+    },
+    {
+        id: 'card-2',
+        src: '/thank-you-card-2.png',
+        alt: "A moosive thank you!",        
+    },
+    {
+        id: 'card-3',
+        src: '/thank-you-card-3.png',
+        alt: "Thank you for teaching me stuff and making me clever!",
+    },
+    {
+        id: 'card-4',
+        src: '/thank-you-card-4.png',
+        alt: "You've only gone and done it. Huge congrats!",        
+    },
+    {
+        id: 'card-5',
+        src: '/thank-you-card-5.png',
+        alt: "Thank you for bee-ing so amazing!",
+    },
+    {
+        id: 'card-6',
+        src: '/thank-you-card-6.png',
+        alt: "Thank you!",
+    },
+];
+
+export default greetingCardData;
