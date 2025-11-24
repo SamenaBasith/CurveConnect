e0693e25e191702d8949e3f6d830ff77bdc9b150
EdLakeNHS <edward.lake1@nhs.net>
2025-11-24 13:28:52 +0000

    Updated hero href attribute for the button to send user to this new page

--- curve-connect/src/components/Hero.jsx
+++ curve-connect/src/components/Hero.jsx
@@ -5,11 +5,13 @@ import Button from "./Button";
 
 export default function Hero() {
   return (
-    <section className="
+    <section
+      className="
       bg-gradient-to-r from-[#02a4d3] to-[#4c00b0]
       py-15 
       text-center
-    ">
+    "
+    >
       <Image
         src="/curve_connect_logo.png"
         alt="Curve Connect Logo"
@@ -18,14 +20,12 @@ export default function Hero() {
         className="mx-auto mt-15"
       />
 
-      <p className="mt-3 text-lg md:text-xl text-white/90 italic" >
+      <p className="mt-3 text-lg md:text-xl text-white/90 italic">
         Share, Connect & Celebrate
       </p>
-       <div className="mt-10">
-      <Button href="/send-card">
-        Send a Card
-      </Button>
-    </div>
+      <div className="mt-10">
+        <Button href="/choose-card">Send a Card</Button>
+      </div>
     </section>
   );
 }
