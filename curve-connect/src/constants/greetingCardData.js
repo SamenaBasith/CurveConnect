594e176be09f878afba0887bf8064c87952f3c75
SamenaBasith <115994259+SamenaBasith@users.noreply.github.com>
2025-11-25 23:02:59 +0000

    changing cards to avoid copyright and updating cardData

--- curve-connect/src/constants/greetingCardData.js
+++ curve-connect/src/constants/greetingCardData.js
@@ -1,34 +1,70 @@
 const greetingCardData = [
-    {
-        id: 'card-1',
-        src: '/thank-you-card-1.png',
-        alt: "I'm ever so grateful!",
-    },
-    {
-        id: 'card-2',
-        src: '/thank-you-card-2.png',
-        alt: "A moosive thank you!",        
-    },
-    {
-        id: 'card-3',
-        src: '/thank-you-card-3.png',
-        alt: "Thank you for teaching me stuff and making me clever!",
-    },
-    {
-        id: 'card-4',
-        src: '/thank-you-card-4.png',
-        alt: "You've only gone and done it. Huge congrats!",        
-    },
-    {
-        id: 'card-5',
-        src: '/thank-you-card-5.png',
-        alt: "Thank you for bee-ing so amazing!",
-    },
-    {
-        id: 'card-6',
-        src: '/thank-you-card-6.png',
-        alt: "Thank you!",
-    },
+  {
+    id: "card-1",
+    src: "/card_templates/thank-you.png",
+    type: "Thank You",
+    alt: "I'm ever so grateful!",
+  },
+  {
+    id: "card-2",
+    src: "/card_templates/birthday.png",
+    type: "Birthday",
+    alt: "Happy Birthday!",
+  },
+  {
+    id: "card-3",
+    src: "/card_templates/smart-cookie.png",
+    type: "Thank You",
+    alt: "Thank you for teaching me stuff and making me clever!",
+  },
+  {
+    id: "card-4",
+    src: "/card_templates/best-colleague.png",
+    type: "Thank You",
+    alt: "Best colleague ever!",
+  },
+  {
+    id: "card-5",
+    src: "/card_templates/congratulations.png",
+    type: "Congratulations",
+    alt: "You've only gone and done it! Huge congrats!",
+  },
+  {
+    id: "card-6",
+    src: "/card_templates/christmas.png",
+    type: "Christmas",
+    alt: "Happy Xmas",
+  },
+  {
+    id: "card-7",
+    src: "/card_templates/for-better.png",
+    type: "Values",
+    alt: "For Better",
+  },
+  {
+    id: "card-8",
+    src: "/card_templates/together.png",
+    type: "Values",
+    alt: "Together",
+  },
+  {
+    id: "card-9",
+    src: "/card_templates/integrity.png",
+    type: "Values",
+    alt: "Integrity",
+  },
+  {
+    id: "card-10",
+    src: "/card_templates/courage.png",
+    type: "Values",
+    alt: "Courage",
+  },
+  {
+    id: "card-11",
+    src: "/card_templates/excellence.png",
+    type: "Values",
+    alt: "Excellence",
+  },
 ];
 
 export default greetingCardData;
